e41476c2e768e78f682a7cedfa1d75d50b4d4248
Caio Silva <caiovosilva@outlook.com>
2019-08-27 14:49:22 -0300

    sending products from file to page

--- store/src/components/Navbar.js
+++ store/src/components/Navbar.js
@@ -11,7 +11,7 @@ export default class Navbar extends Component {
     return (
         <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
 
-        {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+        {/* https://www.iconfinder.com/icons/1243689/call_phone_icon  
         Creative Commons (Attribution 3.0 Unported);
         https://www.iconfinder.com/Makoto_msk */}
           <Link to="/">
