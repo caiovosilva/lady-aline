f460f45094abde035c5862be1e5ec4b79f7fbb32
Caio Silva <caiovosilva@outlook.com>
2019-08-29 09:38:37 -0300

    cart button and product footer

--- store/src/components/Product.js
+++ store/src/components/Product.js
@@ -9,14 +9,30 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={console.log('youclick')}>
+                    <div className="img-container p-5" onClick={() => {console.log('youclick')}}>
                         <Link to="/details">
                             <img src={img} alt="produto" className="card-img-top"/>
-                            
                         </Link>
+                        <button className="cart-btn" disable={inCart? true : undefined}
+                        onClick= {() => { console.log('added to the cart')}}>
+                            {inCart ? (
+                                <p className="text-capitalize mb-0" disabled>
+                                    {" "}
+                                    No Carrinho
+                                </p>
+                            ) : (
+                                <i className="fas fa-cart-plus"/>
+                            )}    
+                        </button>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">{title}</p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">R$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
-                <h3>hello from product</h3>
             </ProductWrapper>
         )
     }
