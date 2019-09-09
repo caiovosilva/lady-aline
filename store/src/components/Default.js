a0decfad6ab091f5507af0c2accc26211002bcba
Caio Silva <caiovosilva@outlook.com>
2019-09-09 08:39:00 -0300

    dealing with 404 error

--- store/src/components/Default.js
+++ store/src/components/Default.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Default () {
-  return (
-    <div>
-      <h3>page not found</h3>
-    </div>
-  )
+export default class Default extends Component {
+  render () {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto text-center text-title text-uppercase pt-5'>
+            <h1 className='display-3'>erro 404</h1>
+            <h2>página não encontrada</h2>
+            <h3>
+              a URL
+              <span className='text-danger'>
+                {this.props.location.pathname}
+              </span>{' '}
+              não foi encotnrada
+            </h3>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
