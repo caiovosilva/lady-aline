8a82c04e4e3984278baa19b6f61788c8085cb306
Caio Silva <caiovosilva@outlook.com>
2019-09-13 11:03:02 -0300

    renaming files.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../images/logo.svg'
+import logo from '../img/phoneIcon.svg'
 import styled from 'styled-components'
 import { ButtonContainer } from './Button'
 
